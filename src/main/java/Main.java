cc4ed9e0460114310aa4d8bce92536590fa7d041
Veider <a.l.e.x.1.9@yandex.ru>
2023-06-27 01:17:12 +0300

    Семинар 3.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,51 +1,33 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
 public class Main {
     public static void main(String [] args) {
-        System.out.println();
-        System.out.println("select * from students where " + domashka1());
-        System.out.println();
-        System.out.println(domashka2());
-    }
-    static StringBuilder dz = new StringBuilder("Name:Ivanov, Country:Russia, City:Moscow, Age:Null");
-    static String result = "0";
-    public static String domashka1() {
-        int res = dz.indexOf("Age:Null");
-        for (int i = 0; i < dz.length(); i++) {
-            if (i < res-2) {
-                if (dz.charAt(i) == ':'){
-                    dz.setCharAt(i,'=');
-                }
-            }
-            else {
-                dz.delete(i,dz.length());
-            }
+        ArrayList <Integer> list = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+        list.add(new Random().nextInt(20));
         }
-        result = dz.toString();
-        return result;
-    }
-
-    static String[] fam = {"Иванов", "Петрова", "Краснов"};
-    static String[] ocenka = {"5", "4", "5"};
-    static String[] predmet = {"Математика", "Информатика", "Физика"};
-    static StringBuilder dz2 = new StringBuilder ("Студент x получил y по предмету z");
-
-    public static Void domashka2() {
-        result = dz2.toString();
-        for (int j = 0; j < 3; j++) {
-            for (int i = 0; i < dz2.length(); i++) {
-                if (dz2.charAt(i) == 'x') {
-                    dz2.delete(i, i+1);
-                    dz2.insert(i, fam[j]);
-                } else if (dz2.charAt(i) == 'y') {
-                    dz2.delete(i, i+1);
-                    dz2.insert(i, ocenka[j]);
-                } else if (dz2.charAt(i) == 'z') {
-                    dz2.delete(i, i+1);
-                    dz2.insert(i, predmet[j]);
-                    System.out.println(dz2);
-                    dz2 = new StringBuilder(result);
-                }
-            }
+        list.sort(Comparator.naturalOrder());
+        for (int i = 0; i < list.size(); i++){
+            System.out.print(list.get(i));
+            System.out.print(" ");
         }
-        return null;
+        int min = list.get(0);
+        int max = list.get(list.size()-1);
+        System.out.println();
+        System.out.println("Минимум равен: " + min);
+        System.out.println("Максимум равен: " + max);
+        int sredneeAr = 0;
+        for (int i = 0; i < list.size(); i++){
+            sredneeAr = sredneeAr+list.get(i);
+        }
+        System.out.println("Среднее арифметическое значение: " + sredneeAr/ list.size());
+        System.out.println("Значение в середине: " + list.get(list.size()/2));
+        list.removeIf(n -> (n%2==0));
+        for (int i = 0; i < list.size(); i++){
+            System.out.print(list.get(i));
+            System.out.print(" ");
+        }
     }
 }
